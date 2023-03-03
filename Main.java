663463d8bb5928771123c2c31d9fc8fd79c1cfa5
Arun pandian <arun07160@gmail.com>
2023-03-03 21:14:25 +0530

    array PROGRAM ADDED

--- Main.java
+++ Main.java
@@ -1,52 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //BOX PATTERN
-//        for (int i=0;i<5;i++) {
-//            for (int j=0;j<5;j++) {
-//                System.out.print("$ ");
-//
-//            }
-//            System.out.println(" ");
-//        }
-//        //RIGHT ANGLE PATTERN
-//        for (int i=0;i<=5;i++) {
-//            for (int j=0;j<i;j++) {
-//                System.out.print("$ ");
-//
-//            }
-//            System.out.println(" ");
-//        }
-//        System.out.println("");
-//        //INVERTEFD RIGHT ANGLE PATTERN
-//        for (int i=0;i<5;i++) {
-//            for (int j=0;j<5-i;j++) {
-//                System.out.print("$ ");
-//
-//            }
-//            System.out.println(" ");
-//        }
-
-        //PYRAMID PATTERN
-        for (int i=0;i<5;i++) {
-            for (int j=0;j<5+i;j++) {
-                if (j<5-i-1){
-                    System.out.print(" ");
-                }
-            else{
-            System.out.print("*");
+            System.out.println("enter the values");
+        Scanner arun=new Scanner(System.in);
+        int []a=new int[arun.nextInt()];
 
 
-            }
-                System.out.print(" ");
+        for(int i=0;i<=10;i++)
+        {
+            a[i]=i*5;
+            System.out.println("the array index a"+i+"is"+a[i]);
 
-            }
         }
-
-
-
     }
-    }
-
-
-
-
+}
