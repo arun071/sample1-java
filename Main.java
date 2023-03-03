1a15e3d55a12df553d2c840dd25bed6aa5afad50
Arun pandian <arun07160@gmail.com>
2023-03-03 21:14:25 +0530

    PATTERN PROGRAM ADDED

--- Main.java
+++ Main.java
@@ -1,22 +1,52 @@
-import java.util.Scanner;
-
 public class Main {
+    public static void main(String[] args) {
+        //BOX PATTERN
+//        for (int i=0;i<5;i++) {
+//            for (int j=0;j<5;j++) {
+//                System.out.print("$ ");
+//
+//            }
+//            System.out.println(" ");
+//        }
+//        //RIGHT ANGLE PATTERN
+//        for (int i=0;i<=5;i++) {
+//            for (int j=0;j<i;j++) {
+//                System.out.print("$ ");
+//
+//            }
+//            System.out.println(" ");
+//        }
+//        System.out.println("");
+//        //INVERTEFD RIGHT ANGLE PATTERN
+//        for (int i=0;i<5;i++) {
+//            for (int j=0;j<5-i;j++) {
+//                System.out.print("$ ");
+//
+//            }
+//            System.out.println(" ");
+//        }
 
-public static void main(String[]args)
-    {
-        System.out.println("enter the average mark");
-        Scanner arun=new Scanner(System.in);
-        int avg= arun.nextInt();
-        if(avg>90&&avg<=100)
-        {
-            System.out.println("your grade is A");
+        //PYRAMID PATTERN
+        for (int i=0;i<5;i++) {
+            for (int j=0;j<5+i;j++) {
+                if (j<5-i-1){
+                    System.out.print(" ");
+                }
+            else{
+            System.out.print("*");
+
+
+            }
+                System.out.print(" ");
+
+            }
         }
-        else if(avg>60&&avg<=89)
-        {
-            System.out.println("your grade is b");
-        }
-        else {
-            System.out.println("your grade is c");
-        }
+
+
+
     }
-}
+    }
+
+
+
+
